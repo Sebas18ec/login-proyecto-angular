323493f2b54b26c11be35a462ba091c91a9c6ada
Sebas18ec <sebasbelal@gmail.com>
2023-04-12 11:18:06 -0500

    Emisores API

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,58 @@
 import { Component } from '@angular/core';
 
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  
+  emisores: any;
+  selectedEmisor: any;
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get<any>('api/ControladorAPI/api/v1/emisores')
+      .subscribe(data => {
+        this.emisores = data;
+        console.log(this.emisores); 
+      });
+  }
+/*  
+  emisores: any[] = [];
+  selectedEmisor: any;
+  username: string = '';
+  password: string = '';
+  
+
+  constructor(private http: HttpClient) { }
+
+  emisorSeleccionado(emisor: any) {
+    this.selectedEmisor = emisor;
+  }
+  
+
+  getEmisores() {
+    const headers = new HttpHeaders().set('Content-Type', 'application/json');
+    this.http.get<any>('api/ControladorAPI/api/v1/emisores', { headers })
+  .subscribe(data => {
+    this.emisores = data.map(emisor => emisor.nombreEmisor);
+  });
+
+  }
+  
+  ngOnInit() {
+    this.getEmisores();
+    
+    
+  }
+  
+  */
+
+  onSubmit() {
+    // Aquí iría la lógica para validar el usuario y la contraseña ingresados
+  }
 }
